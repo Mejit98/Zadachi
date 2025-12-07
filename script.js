df5e2f20b12449b4b6dbd4b89b124e6cf0b7f1b5
Mejit98 <medzitssejthalilov@gmail.com>
2025-12-07 14:57:46 +0300

    zadachi

---
+++ script.js
@@ -0,0 +1,154 @@
+"use strict"; /* строгий режим */
+alert('Хотите закрыть страницу?');
+
+
+
+//let name = prompt('Ваше имя?');
+// alert('Ваше имя: ' + prompt('Ваше имя?'));
+// alert('Ваш возраст: '+prompt('Ваш возраст?'));
+
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+
+// alert(Number(num1) + Number(num2));
+
+// let side_of_square = prompt('Введите сторону квадрата');
+// alert('Площадь квадрата: '+ side_of_square ** 2);
+
+// let side = prompt('Введите первую сторону квдарат');
+// let side2 = prompt('Введите вторую сторону квдарат');
+// alert("Переиметр премяугольника: " + (Number(side) + Number(side2))*2);
+
+
+
+// document.write('<span style="font-style: italic;">Текст курсивом</span>');
+// let str = 'text';
+// document.write('<i>' + str + '</i>');
+
+// document.write('1<br>');
+// document.write('2<br>');
+// document.write('3<br>');
+// document.write('4<br>');
+// document.write('5<br>');
+
+
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + (Number(num1) + Number(num2))); 
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// console.log(a + b + c);
+
+// let num = '123';
+// let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+// console.log(sum);
+
+
+// let num = '123';
+// console.log(String(num[0]));
+
+
+// let a = 0;
+// a++
+// console.log(a);
+
+// let num = '123';
+// console.log(num.length);
+
+// let a = 24;
+// let b =60;
+// let c = 60;
+// let time = a *b*c;
+// console.log(time);
+
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+// let num = '123';
+// let str = num.length -1;
+// console.log(num[str]);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+// let num = '123';
+// let str = num.length -1;
+// console.log(num[str]);
+
+// let a = +'123';
+// let b = +'456';
+// let s = a +b;
+
+// console.log(s);
+
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+// console.log(aaa + bbb + ccc);
+
+
+// let secondsInDay = 24 * 60 * 60;
+// console.log(secondsInDay); 
+
+// let secondsIn30Days = 30 * 24 * 60 * 60;
+// console.log(secondsIn30Days);
+
+
+// let secondsInYear = 365 * 24 * 60 * 60;
+// console.log(secondsInYear);
+
+// let minutesInDay = 24 * 60;
+// console.log(minutesInDay);
+
+// let minutesInYear = 365 * 24 * 60;
+// console.log(minutesInYear);
+
+// let bytesInMegabyte = 1024 * 1024;
+// console.log(bytesInMegabyte);
+
+// let bytesInGigabyte = 1024 * 1024 * 1024;
+// console.log(bytesInGigabyte); 
+
+// let bytesIn10GB = 10 * 1024 * 1024 * 1024;
+// console.log(bytesIn10GB);
+
+
+// let bytesInTerabyte = 1024 * 1024 * 1024 * 1024;
+// console.log(bytesInTerabyte);
+
+// let kilobytesInTerabyte = 1024 * 1024 * 1024;
+// console.log(kilobytesInTerabyte);
+
+// let r = 5; // радиус круга
+// let s = 3.14 * (5**2);
+// console.log(s); 
+
+
+// let a = 4; // сторона квадрата
+// let s = (a**2);
+// console.log(s); 
+
+
+// let a = 3; 
+// let b = 7; 
+// let s = a * b;
+// console.log(s); 
+
+
+// let a = 5; 
+// let b = 3; 
+// let p = 2 * (a + b);
+// console.log(p); 
+
+// let tc = 25; // температура в Цельсиях
+// let tf = (9/5) * tc + 32;
+// console.log(tf); // 77
+
+let tf = 77; // температура в Фаренгейтах
+let tc = (5/9) * (tf - 32);
+console.log(tc); // 25
---
+++ script1.js
@@ -0,0 +1,197 @@
+"use strict"; //строгий режим
+/*alert('text');
+let num = 123;
+alert(num);
+let a, b, c; // объявляем все 3 переменные
+
+// Присваиваем переменным значения:
+a = 1;
+b = 2;
+c = 3;
+alert(a);
+
+let a1 = 10;
+alert(a1);
+a1=20;
+alert(a1);
+
+let sum = 1+2+3;
+alert(sum);
+
+let a2 = 10;
+let b2 = 2;
+alert(a2+b2);
+alert(a2*b2);
+alert(a2/b2);
+
+let c1 = 10;
+let d = 5;
+let result = c1 + d;
+alert(result); */
+
+/*let a =1;
+let b =2;
+let c =3;
+alert(a+b+c);*/
+
+/* let a =10;
+let b =5;
+let c = a-b;
+let d = 7;
+let result = c+d;
+alert(result); */
+
+/* let a = 1.5;
+let b =0.75;
+alert(a+b); */
+
+/* let a = -100;
+alert(a); */
+
+/* let a = 5;
+alert(-a); */ 
+
+/* let a = 13;
+let b = 5;
+alert(a % b); */
+
+/* let a = 2;
+let b = 10;
+alert(a ** b);*/ 
+
+
+////// СТРОКИ ////
+
+let suranme = 'Seythalilov';
+let name = 'Mejit';
+let full_name  = suranme+ ' ' + name;
+alert(full_name); 
+
+/*let str = '!!!';
+alert(str);*/
+ 
+// let str = 'java';
+// let str1 = 'script';
+// alert(str + str1);
+
+
+// let str = 'hello';
+// let str1 = 'world';
+// alert(str + ' ' + str1);
+
+//alert('MejSeyt'.length);
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = 'aaa ${str1} bbb + ${str2} ccc'; 
+// alert(txt);
+
+// let str = `a
+// b
+// c`;
+// alert(str);
+
+// let a;
+// alert(a);
+
+// let a =null;
+// alert(a);
+
+// let a = true;
+// alert(a);
+
+
+// let a = false;
+// alert(a);
+
+
+// alert('abc' * 3);
+
+// alert(10 /0);
+// alert(-10/0);
+
+// let num1 = 123;
+// let num2 = 456;
+// let num3 = 789;
+
+// const PI =3.14;
+
+
+// let a = '10';
+// let b = '20';
+// alert(Number(a) + Number(b));
+
+// alert( 2 + Number('3') );
+// alert( '2' + Number('3') );
+
+// let a = +'2';
+// let b = +'3';
+// alert(a + b); 
+
+// let a = parseInt('5px');
+// let b = parseInt('6px');
+// alert(a+b);
+
+// let a = parseFloat('5.5px');
+// let b = parseFloat('6.25px');
+// alert(a+b + " " + 'px');
+
+// let num1 = 5;
+// let num2 = 2;
+// alert(String(num1) + String(num2)); 
+
+
+// let num = 123456789;
+// alert(String(num).length);
+
+// let num = 1234;
+// let num1 =56789;
+// alert(String(num).length + String(num1).length);
+
+
+// let a = 'abcde';
+// alert(a[0]);
+// alert(a[2]);
+// alert(a[4]);
+
+
+// let str = 'abcde';
+// alert(str[4] + str[3] + str[2] +str[1] +str[0]);
+
+
+// let str = 'abcde';
+// let num =3;
+// alert(str[num]);
+
+// let str = 'asdf';
+// alert(str[str.length - 1]); // последний символ
+
+// let str = 'asdf';
+// alert(str[str.length - 2]); // предпоследний символ
+
+// let str = 'asdf';
+// alert(str[str.length - 3]); // предпредпоследний символ
+
+// let num = '12345';
+// alert(Number(num[0]) + Number(num[1]) + Number(num[2]) + Number(num[3]) +Number(num[4]) );
+
+// let test = String(12345); 
+// alert(Number(test[0]) + Number(test[1]) + Number(test[2])+ Number(test[3]) +Number(test[4])) ; 
+// alert(test[0] * test[1] *test[2] * test[3] * test[4]);
+// alert(test[4] + test[3] + test[2] + test[1] +test[0]);
+
+// let num = 47;
+// num +=7;
+// num-=18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+// alert(0.1 * 0.2);
+// alert(0.3 - 0.1);
